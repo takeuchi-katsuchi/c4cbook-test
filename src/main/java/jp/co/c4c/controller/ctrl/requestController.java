ae0ff1c74ed3500e48f9a2ed4b3edff11587a927
m-kamizono <m.kamizono@c4c.co.jp>
2020-08-01 15:50:39 +0900

    Exceptionになるのでコメント追加（神園）

--- src/main/java/jp/co/c4c/controller/ctrl/requestController.java
+++ src/main/java/jp/co/c4c/controller/ctrl/requestController.java
@@ -1,15 +1,14 @@
 package jp.co.c4c.controller.ctrl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.c4c.controller.form.requestForm;
 import jp.co.c4c.service.SampleService;
 
-@Controller
-@RequestMapping("/")
+//@Controller
+//@RequestMapping("/")
 public class requestController {
 
 	@Autowired
