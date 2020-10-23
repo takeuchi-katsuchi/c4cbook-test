bb9473e431dd71a51b4da7d77d08aa1829dff88b
g-kobari <genkikobari@gmail.com>
2020-10-23 21:48:08 +0900

    リクエスト　作業

--- src/main/webapp/resources/js/request.js
+++ src/main/webapp/resources/js/request.js
@@ -1,32 +1,3 @@
-function readMoreReason(number) {
-
-	//reasonクラスのdivを取得
-	var reasons = document.getElementsByClassName('reason');
-	//readMoreBtnsクラスのdivを取得
-	var readmores = document.getElementsByClassName('readmore');
-
-	//「ReadMore」を非表示にする
-	readmores[number].className += " hide";
-
-	//要望理由をすべて表示する
-	reasons[number].id = "reason_open";
-
-}
-
-function readMoreRejectReason(number) {
-
-	//reason_rejectクラスのdivを取得
-	var reasons_reject = document.getElementsByClassName('reason_reject');
-	//readMoreBtnsクラスのdivを取得
-	var readmores_reject = document.getElementsByClassName('readmore_reject');
-
-	//「ReadMore」を非表示にする
-	readmores_reject[number].className += " hide";
-
-	//要望理由をすべて表示する
-	reasons_reject[number].id = "reason_reject_open";
-
-}
 
  function requestOnload() {
 	// ページ読み込み時に実行したい処理
@@ -34,49 +5,11 @@ function readMoreRejectReason(number) {
  //本の画像のリサイズ
 imgResize();
 
-	//////////要望理由のreadmore//////////
-
-	//reasonクラスのdivを取得
-	var reasons = document.getElementsByClassName('reason');
-	//reasonsクラスを持つ要素の数を取得
-	var i_roasons = reasons.length;
-	//readmoreクラスのdivを取得
-	var readmores = document.getElementsByClassName('readmore');
-
-	for (var i = 0;  i < i_roasons;  i++) {
-
-		//3行以上なければ
-		if (reasons[i].clientHeight < 68) {
-			readmores[i].className += " hide";
-		};
-
-	};
-
-	//////////却下理由のreadmore//////////
-
-	//reason_rejectクラスのdivを取得
-	var reasons_reject = document.getElementsByClassName('reason_reject');
-	//reason_rejectsクラスを持つ要素の数を取得
-	var i_roasons_reject = reasons_reject.length;
-	//readmore_rejectクラスのdivを取得
-	var readmores_reject = document.getElementsByClassName('readmore_reject');
-
-	for (var i = 0;  i < i_roasons_reject;  i++) {
-
-		//3行以上なければ
-		if (reasons_reject[i].clientHeight < 68) {
-			readmores_reject[i].className += " hide";
-		};
-
-	};
 }
 
 //////////モーダル//////////
 
 $(document).ready(function() {
-
- console.log("JQuery OK");
-
 
  $(document).on('click', `#openLendingProcedureModal`, function() {
      $(`#LendingProcedureModal`).modal('show');
@@ -137,8 +70,6 @@ function cheerBook(requestId,memberId,requestStatus,test){
     },
     error: function() {
      alert("要望は各本につき1度だけ応援できます。");
-//     $(`#myModal2`).modal('hide');
-//     $('#LendingProcedureModal').modal('hide');
     }
    });
   }
@@ -161,9 +92,9 @@ function requestBook(memberId){
 
    var formData = {
    title: document.getElementById('inpt-title').value,
-   titleKana: document.getElementById('inpt-title').value,
+   titleKana: document.getElementById('inpt-title-kana').value,
    author: document.getElementById('inpt-author').value,
-   authorKana: document.getElementById('inpt-author').value,
+   authorKana: document.getElementById('inpt-author-kana').value,
    memId: memberId,
    comment: document.getElementById('textarea').value,
    requestStatus: 0
@@ -179,7 +110,8 @@ function requestBook(memberId){
     dataType: 'json',
     success: function(response) {
      //モーダルの入力欄全クリアしてモーダルを閉じる
-     alert("sucsses：" + response);
+     clearInputValue();
+     $('#LendingProcedureModal').modal('hide');
     },
     error: function() {
      alert("error");
@@ -188,7 +120,57 @@ function requestBook(memberId){
   }
 }
 
+//////////要望後入力欄クリア//////////
+function clearInputValue() {
+    var arrayId =new Array('inpt-title', 'inpt-title-kana', 'inpt-author', 'inpt-author-kana', 'textarea');
+
+    for (let i = 0; i < arrayId.length; i++) {
+      document.getElementById(arrayId[i]).value = '';
+    }
+}
+
+//////////xss対策用エスケープ処理//////////
+//function escapeHTML(string){
+//  return string.replace(/\&/g, '&amp;')
+//    .replace(/\</g, '&lt;')
+//    .replace(/\>/g, '&gt;')
+//    .replace(/\"/g, '&quot;')
+//    .replace(/\'/g, '&#x27');
+//}
 
 
 
 
+$('.reason-text').each(function(e){
+
+    let $comment = $(this);
+    // 元の文章を変数に格納
+    let comment = $comment.html();
+    let length = comment.length;
+    let commentShow;
+    let commentText;
+    if($comment.height() > 50){
+      // 文章の要素の高さが50を超えていたら隠す用のisHiddenクラスを付与
+      $comment.addClass('isHidden');
+
+      commentShow = comment.replace(/<br>/g, "\u00a0").substring(0, 68);
+      commentText = commentShow += '<span class="review-contents-more">' + '... 続きを読む' + '</span>';
+      $comment.html(commentText);
+
+      // 続きを読むをクリックで元の文章を表示
+      $comment.on('click', '.review-contents-more', function(e){
+        let $anchor = $(e.currentTarget);
+        let $anchorParent = $anchor.parent();
+        $anchorParent.removeClass('isHidden');
+        $anchorParent.html(''); // 一旦空にする
+        $anchorParent.html(comment).append('<BR><span class="review-contents-hide">' + '閉じる' + '</span>');
+        // 閉じるをクリックで元に戻す
+        $('.review-contents-hide').on('click', function(e){
+          let $anchorHide = $(e.currentTarget).parent();
+          $anchorHide.addClass('isHidden');
+          $anchorHide.html('');
+          $anchorHide.html(commentText);
+        });
+      });
+    }
+});
