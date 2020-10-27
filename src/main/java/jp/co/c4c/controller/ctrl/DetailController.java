63505f682fa3235cf1e38b3fdea0e10046b0fa1c
takayukiyamaoka <tkykymok@me.com>
2020-10-28 08:29:11 +0900

    詳細ページ　おすすめ・レビューモーダル　おすすめする人をチェックボックスで選択できるように修正 + ログインユーザー自身を選択肢から除外

--- src/main/java/jp/co/c4c/controller/ctrl/DetailController.java
+++ src/main/java/jp/co/c4c/controller/ctrl/DetailController.java
@@ -1,8 +1,11 @@
 package jp.co.c4c.controller.ctrl;
 
-import java.util.Iterator;
-import java.util.List;
-
+import jp.co.c4c.controller.form.DetailForm;
+import jp.co.c4c.db.dto.BK_M_MemBasicDto;
+import jp.co.c4c.db.dto.V_LendHistoryDto;
+import jp.co.c4c.db.dto.WebSessionDto;
+import jp.co.c4c.service.CommonService;
+import jp.co.c4c.service.DetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import jp.co.c4c.controller.form.DetailForm;
-import jp.co.c4c.db.dto.V_LendHistoryDto;
-import jp.co.c4c.db.dto.WebSessionDto;
-import jp.co.c4c.service.CommonService;
-import jp.co.c4c.service.DetailService;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/detail")
@@ -40,12 +41,17 @@ public class DetailController {
         boolean isLogined = commonService.isLogined(webSessionDto);
         if (!isLogined) return "redirect:login";
 
-        // 全メンバー
-        form.setBk_M_MemBasicDtoList(detailService.getAllMembers());
+        // 全メンバー取得してログインユーザー自身を除外してformにセット（おすすめする人の選択肢表示用）
+        int memId = webSessionDto.getMemId();
+        List<BK_M_MemBasicDto> bk_m_memBasicDtoList = detailService.getAllMembers().stream()
+                .filter(obj -> obj.getMemId() != memId)
+                .collect(Collectors.toList());
+        form.setBk_M_MemBasicDtoList(bk_m_memBasicDtoList);
+
         // 対象の本
         form.setV_TopAndDetailDto(detailService.getBookById(bookId));
 
-        // 貸出履歴　返却ステータス以外を除外
+        // 貸出履歴　返却ステータス以外を除外してformにセット
         List<V_LendHistoryDto> lendHistoryDtoList = detailService.getLendHistorysByBookId(bookId);
         Iterator<V_LendHistoryDto> iterator = lendHistoryDtoList.iterator();
         while (iterator.hasNext()) {
@@ -60,8 +66,6 @@ public class DetailController {
         // おすすめしてる人
         form.setV_RecomMemDtoList(detailService.getRecomMembersById(bookId));
         return "detail";
-
-
     }
 
 }
